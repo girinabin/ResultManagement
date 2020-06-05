4d5ad3576cf97bf18947319ba3117b4b0358a076
giri.nabin1994 <giri.nabin1994@gmail.com>
2020-06-05 15:13:40 +0545

    added

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,3 +23,4 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/dashboard','DashboardController@index')->name('dashboard');
 
+Route::get('/test','DashboardController@test');
